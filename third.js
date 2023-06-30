3e3e62ed1b2a66b808c541bb2fbcdcea0c393a75
NIttaDragon <101250164+NIttaDragon@users.noreply.github.com>
2023-06-30 12:09:27 +0300

    Update third.js

--- third.js
+++ third.js
@@ -1,12 +1,17 @@
+//переменные
+
+let square = document.querySelector('.square');
+let circle = document.querySelector('.circle');
+// let elem = circle.cloneNode(true);
+
+//функции
+
 function setColor1(e){
   if(e.type == 'mouseover')
     e.target.style.backgroundColor = 'red';
   else if(e.type == 'mouseout')
     e.target.style.backgroundColor = 'orange';
 }
-var square = document.querySelector('.square');
-square.addEventListener('mouseover', setColor1);
-square.addEventListener('mouseout', setColor1);
 
 function setColor2(e){
   if(e.type == 'mouseover')
@@ -14,18 +19,20 @@ function setColor2(e){
   else if(e.type == 'mouseout')
     e.target.style.backgroundColor = 'green';
 }
-var circle = document.querySelector('.circle');
-circle.addEventListener('mouseover', setColor2);
-circle.addEventListener('mouseout', setColor2);
 
 document.querySelector('.circle').onclick = function clonei(event){
-  var fig = document.querySelector('.figures');
-  var elemy = document.querySelector('.circle');
-  var elem = elemy.cloneNode(true);
+  let elem = circle.cloneNode(true);
   document.body.appendChild(elem);
 }
 
 document.querySelector('.square').onclick = function dely(event){
-  var elemy = document.querySelector('.square');
-  elemy.parentNode.removeChild(elemy);
+  square.parentNode.removeChild(square);
 }
+
+//применение
+
+square.addEventListener('mouseover', setColor1);
+square.addEventListener('mouseout', setColor1);
+
+circle.addEventListener('mouseover', setColor2);
+circle.addEventListener('mouseout', setColor2);
